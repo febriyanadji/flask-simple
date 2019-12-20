00a7c119cd5d59e621ee3cb7c4547eca51f8d294
febriyna <febriyan.aji@gmail.com>
2019-12-20 09:20:26 +0700

    tambah pytest

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'hai <b> febri</b>, ini perubahan'
+    return 'hai <b> febri</b>, ini perubahan, semoga saja'
 if __name__ == '__main__':
     app.run();
