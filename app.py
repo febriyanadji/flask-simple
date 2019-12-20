9fe9b864f6c549ff84db18a19b0fbf7b1755656f
febriyna <febriyan.aji@gmail.com>
2019-12-20 08:45:51 +0700

    perubahan

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'hai <b> febri</b>'
+    return 'hai <b> febri</b>, ini perubahan'
 if __name__ == '__main__':
     app.run();
